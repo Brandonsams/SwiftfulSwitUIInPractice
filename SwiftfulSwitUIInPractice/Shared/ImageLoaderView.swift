6c62de65921d476b791e16c24e4146fc9c5ccac0
brandonsams <brandonkeithsams@gmail.com>
2024-08-04 12:44:05 -0600

    Initial commit

---
+++ SwiftfulSwitUIInPractice/Shared/ImageLoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLoaderView.swift
+//  SwiftfulSwitUIInPractice
+//
+//  Created by Brandon Sams on 8/4/24.
+//
+
+import SwiftUI
+
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
