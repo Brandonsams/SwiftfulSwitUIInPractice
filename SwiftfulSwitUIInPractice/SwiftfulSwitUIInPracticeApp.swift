43ab3d938e32cebcfe52805edc3dae5aecfbb372
brandonsams <brandonkeithsams@gmail.com>
2024-08-04 12:35:37 -0600

    Initial Commit

---
+++ SwiftfulSwitUIInPractice/SwiftfulSwitUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwitUIInPracticeApp.swift
+//  SwiftfulSwitUIInPractice
+//
+//  Created by Brandon Sams on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwitUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
