de5813e73f03765b8743cc7933c00594a3ed6e50
brandonsams <brandonkeithsams@gmail.com>
2024-08-04 13:54:34 -0600

    Actually committing this time lol

--- SwiftfulSwitUIInPractice/Shared/ImageLoaderView.swift
+++ SwiftfulSwitUIInPractice/Shared/ImageLoaderView.swift
@@ -5,14 +5,29 @@
 //  Created by Brandon Sams on 8/4/24.
 //
 
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ImageLoaderView: View {
+    var urlString: String = ""
+    var resizingMode: ContentMode = .fill
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .opacity(0.001)
+            .overlay(WebImage(url: URL(string: Constants.randomImage))
+                .resizable()
+                .indicator(.activity)
+                .aspectRatio(contentMode: .fill)
+                .allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
+            )
+            .clipped()
     }
 }
 
 #Preview {
     ImageLoaderView()
+        .cornerRadius(30)
+        .padding(40)
+        .padding(.vertical, 60)
 }
