0fd7284e07c936633f65d358d9462888830faf8e
brandonsams <brandonkeithsams@gmail.com>
2024-08-04 22:37:02 -0600

    Added some views for playlist

--- SwiftfulSwitUIInPractice/SwiftfulSwitUIInPracticeApp.swift
+++ SwiftfulSwitUIInPractice/SwiftfulSwitUIInPracticeApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SwiftfulSwitUIInPracticeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            SpotifyHomeView()
         }
     }
 }
